8823af2091c25b2203d3a8458283b70e1b0d3f44
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-01-24 22:27:56 +0100

    Delete index.js

--- dice/index.js
+++
@@ -1,9 +0,0 @@
-imageArray = ['images/dice1.png', 'images/dice2.png', 'images/dice3.png', 'images/dice4.png', 'images/dice5.png', 'images/dice6.png',];
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-
-console.log(randomNumber1);
-console.log(imageArray[randomNumber1 - 1]);
-
-var newImage = imageArray[randomNumber1 - 1];
-
-document.querySelector('.dice .img1').setAttribute('src',newImage);
