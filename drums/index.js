1e5acad923e6dc90058aa9959a3564e8e9721c88
afminmax <allen.frasier@aquinix.com>
2020-01-25 21:24:48 +0100

    drum pics added

--- drums/index.js
+++ drums/index.js
@@ -1,5 +1,28 @@
-document.querySelector('button').addEventListener('click', handleClick);
+// document.querySelector('button').addEventListener('click', handleClick);
 
-function handleClick() {
-  alert('clique!');
+// function handleClick() {
+//   alert('clique!');
+// }
+
+// document.querySelector('button').addEventListener('click', function() {
+//   //this is an anonymous function
+//   alert('clique!');
+// });
+
+// document.querySelectorAll('button').addEventListener('click', function() {
+//   alert('serenity adams!');
+// });
+var audio = new Audio('sounds/tom-3.mp3');
+var buttonCollection = document.querySelectorAll('.drum');
+var buttonCollectionArray = Array.from(buttonCollection);
+buttonCollectionArray.forEach(function(item) {
+  item.addEventListener('click', play);
+});
+
+function play() {
+  console.log((this.style.color = 'white'));
 }
+
+// function play() {
+//   audio.play();
+// }
