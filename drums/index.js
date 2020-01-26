2db4a412a828cd72e2816f59bf342ec9f207e70e
afminmax <allen.frasier@aquinix.com>
2020-01-26 17:36:46 +0100

    added key  event listeners

--- drums/index.js
+++ drums/index.js
@@ -6,39 +6,42 @@ var buttonCollectionArray = Array.from(buttonCollection);
 buttonCollectionArray.forEach(function(item) {
   item.addEventListener('click', function() {
     var buttonInnerHTML = this.innerHTML; //note: 'this' used to capture the specific key press
-    switch (buttonInnerHTML) {
-      case 'w':
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-      case 'a':
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-      case 's':
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-      case 'd':
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-      case 'j':
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-      case 'k':
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-      case 'l':
-        var kick = new Audio('sounds/kick-bass.mp3');
-        kick.play();
-        break;
-      default:
-        console.log(buttonInnerHTML);
-    }
+    letter = buttonInnerHTML;
+    playAudio(letter);
   });
 });
 
-// function play() {
-//   audio.play();
-// }
+document.addEventListener('keydown', function(event) {
+  //   alert(event.key + ' was pressed');
+  letter = event.key;
+  playAudio(letter);
+});
 
-// function play() {
-//     console.log((this.style.color = 'white'));
-//   }
+function playAudio(letter) {
+  switch (letter) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default:
+      console.log(letter);
+  }
+}
