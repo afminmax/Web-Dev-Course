8f01c6baf58d77ea81de54196bd6adc202e1d870
afminmax <allen.frasier@aquinix.com>
2020-01-26 16:57:26 +0100

    added sound switch

--- drums/index.js
+++ drums/index.js
@@ -1,28 +1,44 @@
-// document.querySelector('button').addEventListener('click', handleClick);
-
-// function handleClick() {
-//   alert('clique!');
-// }
-
-// document.querySelector('button').addEventListener('click', function() {
-//   //this is an anonymous function
-//   alert('clique!');
-// });
-
-// document.querySelectorAll('button').addEventListener('click', function() {
-//   alert('serenity adams!');
-// });
-var audio = new Audio('sounds/tom-3.mp3');
+// select the elements that have the drum class:
 var buttonCollection = document.querySelectorAll('.drum');
+// convert the returned html-collection into an array:
 var buttonCollectionArray = Array.from(buttonCollection);
+// add an event-listener to each element withthe forEach method:
 buttonCollectionArray.forEach(function(item) {
-  item.addEventListener('click', play);
+  item.addEventListener('click', function() {
+    var buttonInnerHTML = this.innerHTML; //note: 'this' used to capture the specific key press
+    switch (buttonInnerHTML) {
+      case 'w':
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      case 'a':
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+      case 's':
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+      case 'd':
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+      case 'j':
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+      case 'k':
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+      case 'l':
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+      default:
+        console.log(buttonInnerHTML);
+    }
+  });
 });
-
-function play() {
-  console.log((this.style.color = 'white'));
-}
 
 // function play() {
 //   audio.play();
 // }
+
+// function play() {
+//     console.log((this.style.color = 'white'));
+//   }
