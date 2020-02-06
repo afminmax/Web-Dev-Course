7ffaa5a42afa9a169346274ef22fc4c94ab74874
afminmax <allen.frasier@aquinix.com>
2020-02-06 21:37:41 +0100

    added sounds

--- simon/game.js
+++ simon/game.js
@@ -1,13 +1,47 @@
 gamePattern = [];
 buttonColors = ['red', 'blue', 'green', 'yellow'];
 
+// $('#red').css('background-color', 'pink');
+
+function playAudio(color) {
+  switch (color) {
+    case 'red':
+      var red = new Audio('sounds/red.mp3');
+      red.play();
+    case 'blue':
+      var blue = new Audio('sounds/blue.mp3');
+      blue.play();
+    case 'green':
+      var green = new Audio('sounds/green.mp3');
+      green.play();
+    case 'yellow':
+      var yellow = new Audio('sounds/yellow.mp3');
+      yellow.play();
+      break;
+    default:
+    // console.log(letter);
+  }
+}
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push[randomChosenColor];
+
   return randomChosenColor;
 }
 
-$('.testbtn').css('background-color', 'pink');
-$('#red').css('background-color', 'pink');
-// $('');
+function invokeAV(color) {
+  playAudio(color);
+  $('#' + color)
+    .fadeOut(100)
+    .fadeIn(100);
+}
+
+color = nextSequence();
+invokeAV(color);
+console.log(color);
+
+// $('#' + color)
+//   .fadeOut(100)
+//   .fadeIn(100);
