f93e755a038f25a3d35c5a432ef01f6be53ed30e
afminmax <allen.frasier@aquinix.com>
2020-02-07 17:53:32 +0100

    added initial game logic

--- simon/game.js
+++ simon/game.js
@@ -1,53 +1,93 @@
 gamePattern = [];
 userClickedPattern = [];
-// buttonColors = ['red', 'blue', 'green', 'yellow'];
+buttonColors = ['red', 'blue', 'green', 'yellow'];
+var randomChosenColor = '';
+var level = 0;
 
-// // $('#red').css('background-color', 'pink');
+// function to play audio of the button
+function playAudio(color) {
+  switch (color) {
+    case 'red':
+      var red = new Audio('sounds/red.mp3');
+      red.play();
+    case 'blue':
+      var blue = new Audio('sounds/blue.mp3');
+      blue.play();
+    case 'green':
+      var green = new Audio('sounds/green.mp3');
+      green.play();
+    case 'yellow':
+      var yellow = new Audio('sounds/yellow.mp3');
+      yellow.play();
+      break;
+    default:
+    // console.log(letter);
+  }
+}
 
-// function playAudio(color) {
-//   switch (color) {
-//     case 'red':
-//       var red = new Audio('sounds/red.mp3');
-//       red.play();
-//     case 'blue':
-//       var blue = new Audio('sounds/blue.mp3');
-//       blue.play();
-//     case 'green':
-//       var green = new Audio('sounds/green.mp3');
-//       green.play();
-//     case 'yellow':
-//       var yellow = new Audio('sounds/yellow.mp3');
-//       yellow.play();
-//       break;
-//     default:
-//     // console.log(letter);
-//   }
-// }
+// function to get a random color (runs only once)
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  level = level + 1;
+  console.log(
+    'next random color: ' + randomChosenColor + ' and level: ' + level
+  );
+  return randomChosenColor;
+}
 
-// function nextSequence() {
-//   var randomNumber = Math.floor(Math.random() * 4);
-//   var randomChosenColor = buttonColors[randomNumber];
-//   gamePattern.push[randomChosenColor];
+// function to flash a button
+function flashButton(color) {
+  $('#' + color)
+    .fadeOut(100)
+    .fadeIn(100);
+}
 
-//   return randomChosenColor;
-// }
+// function to animate on
+function animatePress(color) {
+  $('#' + color).addClass('pressed');
+  flashButton(color);
+  setTimeout(function() {
+    $('#' + color).removeClass('pressed');
+  }, 100);
+}
 
-// function invokeAV(color) {
-//   playAudio(color);
-//   $('#' + color)
-//     .fadeOut(100)
-//     .fadeIn(100);
-// }
+// function to check answer
+function checkAnswer() {
+  if (JSON.stringify(userClickedPattern) === JSON.stringify(gamePattern)) {
+    console.log('correct!, next round!');
+  } else {
+    console.log('game over');
+    //reset level
+    //reset both arrays
+    //set header back to original
+  }
+}
 
-// check which button is pressed and pass the value to a variable
+// key event handler - starts the game
+$('body').keypress(function(e) {
+  color = nextSequence();
+  console.log('step1: ' + color);
+  animatePress(color);
+  playAudio(color);
+  $('h1').text('Level 0');
+  console.log('game pattern: ' + gamePattern);
+});
+
+// button click via mouse handler - continues the game
 $('.container .row')
   .find('.btn')
   .click(function(e) {
-    console.log('click happened');
     var userChosenColor = $(this).attr('id');
-    console.log(userChosenColor);
+    console.log('user clicked: ' + userChosenColor);
+    playAudio(userChosenColor);
+    animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
-    return userChosenColor;
+    console.log('user pattern: ' + userClickedPattern);
+    $('h1').text('Level ' + level);
+    checkAnswer();
+    nextSequence();
   });
 
 // color = nextSequence();
