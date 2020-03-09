12dc03a5f334974961f6722e210950e4b1ee784a
afminmax <allen.frasier@aquinix.com>
2020-03-09 21:28:50 +0100

    added updates

--- fruitsProjectMongoose/app.js
+++ fruitsProjectMongoose/app.js
@@ -60,18 +60,30 @@ const peopleSchema = new mongoose.Schema({
 // Step 2: Create a mongoose model, with a collection called peeople (first paramater) using the people schema in step 1.
 const Person = mongoose.model('Person', peopleSchema); //Note: name the collection in singular!
 
-// Step 3: Creating a new person:
-const person = new Person({
-  fname: 'Rebel',
-  lname: 'Sin',
-  email: 'rs@rebelalliance.net',
-  age: 45,
-  occupation: 'X-Wing Pilot'
-});
+// // Step 3: Creating a new person:
+// const person = new Person({
+//   fname: 'Rebel',
+//   lname: 'Sin',
+//   email: 'rs@rebelalliance.net',
+//   age: 45,
+//   occupation: 'X-Wing Pilot'
+// });
 
-// Step 4: Saving the fruit to the mongo db:
-person.save();
-// mongoose.connection.close();
+// // Step 4: Saving the person to the mongo db:
+// person.save();
+
+// Step 5: Updating the person in the mongo db:
+Person.updateOne(
+  { _id: '5e66a48d36a5672c088a1fff' },
+  { fname: 'Rheebel' },
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('record updated');
+    }
+  }
+);
 
 // ------------------- PEOPLE EXAMPLE END -------------------------- //
 
