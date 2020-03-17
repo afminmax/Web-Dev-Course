630ba685270ec41a1390b26811803ecb12cb261f
afminmax <allen.frasier@aquinix.com>
2020-03-18 00:18:23 +0100

    post sums to page in raw complete

--- gw2-api/app.js
+++ gw2-api/app.js
@@ -17,52 +17,58 @@ let naGold = 0;
 let euGold = 0;
 let goldSum = 0;
 
-fs.readFile(keyPath + 'key-na.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  //   console.log('NA key: ' + data);
-  let naKey = data;
-  let url = apiEndpointRoot + data;
-  //   console.log(naApiUrl);
+app.post('/', function(req, res) {
+  fs.readFile(keyPath + 'key-na.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+    //   console.log('NA key: ' + data);
+    let naKey = data;
+    let url = apiEndpointRoot + data;
+    //   console.log(naApiUrl);
 
-  //   console.log('starting api call.... ');
+    //   console.log('starting api call.... ');
 
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      //   console.log(data);
-      let gold = data[0].value;
-      //   console.log(gold);
-      naGold = gold;
-      goldSum = goldSum + naGold;
-      console.log('NA Gold: ' + naGold);
-      console.log('Final Gold Sum: ' + goldSum);
-    });
-});
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        //   console.log(data);
+        let gold = data[0].value;
+        //   console.log(gold);
+        naGold = gold;
+        goldSum = goldSum + naGold;
+        console.log('NA Gold: ' + naGold);
+        console.log('Final Gold Sum: ' + goldSum);
+        res.write('<h3>NA Gold: ' + naGold + '</h3>');
+        res.write('<h3>Final Gold: ' + goldSum + '</h3>');
+        res.send();
+      });
+  });
 
-fs.readFile(keyPath + 'key-eu.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  //   console.log('EU key: ' + data);
-  let euKey = data;
-  let url = apiEndpointRoot + data;
-  //   console.log(euApiUrl);
+  fs.readFile(keyPath + 'key-eu.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+    //   console.log('EU key: ' + data);
+    let euKey = data;
+    let url = apiEndpointRoot + data;
+    //   console.log(euApiUrl);
 
-  //   console.log('starting api call.... ');
+    //   console.log('starting api call.... ');
 
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      //   console.log(data);
-      let gold = data[0].value;
-      //   console.log(gold);
-      euGold = gold;
-      goldSum = goldSum + euGold;
-      console.log('EU Gold: ' + euGold);
-      //   console.log('Gold Sum: ' + goldSum);
-    });
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        //   console.log(data);
+        let gold = data[0].value;
+        //   console.log(gold);
+        euGold = gold;
+        goldSum = goldSum + euGold;
+        console.log('EU Gold: ' + euGold);
+        res.write('<h3>EU Gold: ' + euGold + '</h3>');
+        //   console.log('Gold Sum: ' + goldSum);
+      });
+  });
 });
 
 app.get('/', function(req, res) {
