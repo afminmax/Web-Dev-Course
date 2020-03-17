bed645f47afa75e17383924c1a7c20cf5c5ab936
afminmax <allen.frasier@aquinix.com>
2020-03-17 22:57:45 +0100

    api stuck at 301

--- gw2-api/app.js
+++ gw2-api/app.js
@@ -10,18 +10,33 @@ const keyPath =
 const apiEndpointRoot =
   'https://api.guildwars2.com/v2/account/wallet/?access_token=';
 
+var naApiUrl = '';
+
 fs.readFile(keyPath + 'key-na.txt', 'utf-8', (err, data) => {
   if (err) throw err;
-  console.log('NA key: ' + data);
+  //   console.log('NA key: ' + data);
   let naKey = data;
-  let naApiCall = apiEndpointRoot + data;
-  console.log(naApiCall);
+  let url = apiEndpointRoot + data;
+  //   console.log(naApiUrl);
+
+  console.log('starting api call.... ');
+
+  https.get(url, function(apiResponse) {
+    console.log('submitting the following url: ' + url);
+    console.log('the get status response code is: ' + apiResponse.statusCode);
+    apiResponse.on('data', function(data) {
+      const accountData = JSON.parse(data);
+      console.log(accountData);
+    });
+  });
 });
 
-fs.readFile(keyPath + 'key-eu.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log('EU key: ' + data);
-  let euKey = data;
-  let euApiCall = apiEndpointRoot + data;
-  console.log(euApiCall);
-});
+// fs.readFile(keyPath + 'key-eu.txt', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log('EU key: ' + data);
+//   let euKey = data;
+//   let euApiUrl = apiEndpointRoot + data;
+//   console.log(euApiUrl);
+// });
+
+// 0.value
