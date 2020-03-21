bd9c3ac016004333ad36e5db0d55f3b37cc10899
afminmax <allen.frasier@aquinix.com>
2020-03-21 17:22:37 +0100

    finished on this with comments

--- js-adv-scope/code.js
+++ js-adv-scope/code.js
@@ -1,17 +1,38 @@
-var name = 'cole';
-var handle = '@kneekneemeow.com';
+// // 1 - View how the variables and function are handled on execution
+// // This first example shows the variables as undefined from the top
 
-console.log('name: ', name);
-console.log('handle: ', handle);
-console.log('getUser : ', getUser);
+// console.log('name: ', name);
+// console.log('handle: ', handle);
+// console.log('getUser : ', getUser);
 
-function getUser(handle) {
-  var someUrl = 'http://blah';
-  return someUrl + handle;
-}
+// var name = 'cole';
+// var handle = '@kneekneemeow.com';
 
-console.log(getUser(handle));
+// function getUser(handle) {
+//   var someUrl = 'http://blah';
+//   return someUrl + handle;
+// }
 
+// console.log(getUser(handle));
+
+// // 2 - View how the variables and function are handled on execution
+// // This first example shows the variables become defined
+
+// var name = 'cole';
+// var handle = '@kneekneemeow.com';
+
+// console.log('name: ', name);
+// console.log('handle: ', handle);
+// console.log('getUser : ', getUser);
+
+// function getUser(handle) {
+//   var someUrl = 'http://blah';
+//   return someUrl + handle;
+// }
+
+// console.log(getUser(handle));
+
+//// 3 - Something arcane. Visualize  functions popping on / off an execution stack
 // function a() {
 //   console.log('In function a');
 
@@ -26,3 +47,42 @@ console.log(getUser(handle));
 //   b();
 // }
 // a();
+
+//// 4 - Scope example 1. "bar" is not in the same scope and will generate an error
+// function foo() {
+//   var bar = 'declared in foo';
+// }
+
+// foo();
+// console.log(bar);
+
+// // 5 - Scope example 2. "bar" is not in the same scope and will generate an error
+// console.log('bar initial: ', bar);
+// var bar = '';
+// console.log('bar after declaration: ', bar);
+
+// function foo() {
+//   bar = 'declared in foo';
+// }
+
+// foo();
+// console.log('bar after function call: ', bar);
+
+// // 6 - Scope example 3. Scope chain, the name is changed dynamically
+// function first() {
+//   var name = 'Kyler';
+//   console.log(name);
+// }
+
+// function second() {
+//   var name = 'Emily';
+//   console.log(name);
+// }
+
+// console.log(name);
+// var name = 'Anya';
+// first();
+// second();
+// console.log(name);
+
+var count = 0;
