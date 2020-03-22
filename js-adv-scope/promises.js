798b143730cfe749a374b389e6d39187b1fb5520
afminmax <allen.frasier@aquinix.com>
2020-03-22 18:03:28 +0100

    simplified to arrow functions

--- js-adv-scope/promises.js
+++ js-adv-scope/promises.js
@@ -1,16 +1,64 @@
-let url = 'https://api.guildwars2.com/v2/commerce/listings?ids=19683,19688';
+let url = 'http://api.guildwars2.com/v2/commerce/prices?ids=19683,19684';
 console.log('hello');
 
-let promise = fetch(url); // fetch data from the url above
-promise.then(gotData); // when promise "fulfilled", call the gotData function
-promise.catch(gotErr);
+// // STEP 1: BRUTE FORCE -----------------------------------------------------------------------
+// this function displays the data from a fetch. functions when successful
 
-// this function displays the data. function when successful
-function gotData(data) {
-  console.log(data);
-}
+// function gotData(data) {
+//   console.log(data);
+// }
 
-// this function displays errors. function when there is an error
-function gotErr(err) {
-  console.log(err);
-}
+// // this function displays errors from a fetch. functions when there is an error
+// function gotErr(err) {
+//   console.log(err);
+// }
+
+// let promise = fetch(url); // fetch data from the url above
+// promise.then(gotData); // when promise "fulfilled", call the gotData function
+// promise.catch(gotErr);
+
+// STEP 2: CHAINING THE THEN AND CATCH  ---------------------------------------------------------
+// function gotData(data) {
+//   console.log(data);
+// }
+
+// // // this function displays errors from a fetch. functions when there is an error
+// function gotErr(err) {
+//   console.log(err);
+// }
+
+// fetch(url)
+//   .then(gotData)
+//   .catch(gotErr);
+
+// STEP 3: USING ANON FUNCTIONS ---------------------------------------------------------
+// one can sequence the then and catch with their associated functions:
+// fetch(url)
+//   .then(function gotData(data) {
+//     console.log(data);
+//   })
+//   .catch(function gotErr(err) {
+//     console.log(err);
+//   });
+
+// STEP 4: USING ANON FUNCTIONS ---------------------------------------------------------
+// convert to simpler syntax with arrow functions
+// fetch(url)
+//   .then(data => console.log(data)) //fulfilled
+//   .catch(err => console.log(err)); //error is console logged
+
+// STEP 5: GET THE ACTUAL JSON DATA ---------------------------------------------------------
+// but... we need the data not in raw form but in readable json:
+// no call back hell!
+// fetch(url)
+//   .then(response => response.json()) // first get the fulfilled response and convert it to json via the json method
+//   .then(json => console.log(json)) // show the json
+//   .catch(err => console.log(err)); //error is console logged
+
+// STEP 6: GET SPECIFIC JSON DATA ---------------------------------------------------------
+// now lets get an actual value out of the json nest:
+fetch(url)
+  .then(response => response.json()) // first get the fulfilled response and convert it to json via the json method
+  .then(json => console.log(json)) // show the json
+  // .then(gold => console.log(json.data[0].buys.unit_price))
+  .catch(err => console.log(err)); //error is console logged
