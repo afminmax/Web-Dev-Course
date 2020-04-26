07e9940575d1ca57e5e9ef1e920e49132a1f87d1
afminmax <allen.frasier@aquinix.com>
2020-04-26 14:53:03 +0200

    base keeper done

---
+++ react-keeper/keeper/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// var date = new Date();
+
+function Footer() {
+  return <p>Copyright 2020</p>;
+}
+
+export default Footer;
+
+// {date.getFullYear()}
---
+++ react-keeper/keeper/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div>
+      <h2>Some Note Title Text Here</h2>
+      <p>Some paragraph content here</p>
+    </div>
+  );
+}
+
+export default Note;
